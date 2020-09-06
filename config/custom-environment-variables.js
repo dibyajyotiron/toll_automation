522ee3c0f60d2a0d61ebbba153ac098441dc2632
dibyajyoti <dibyajyoti.beingmyself.ghosal@gmail.com>
2020-09-06 19:03:58 +0530

    config added

---
+++ config/custom-environment-variables.js
@@ -0,0 +1,7 @@
+module.exports = {
+	tokens: {
+		admin: "adminToken",
+		toll: "tollToken",
+	},
+	mongoURI: "mongoURI",
+};
---
+++ config/default.js
@@ -0,0 +1,5 @@
+module.exports = {
+	mongoURI: "mongodb://localhost:27017/toll_plaza",
+	secretOrKey: "secret",
+	directions: ["north", "south", "roundAllowed"],
+};
